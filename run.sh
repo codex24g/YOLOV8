0a81972f3aaa6e22fd7735bc1e25d05f9658a8db
codex24g <yogkan2022@gmail.com>
2024-09-16 14:52:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Install the missing libGL.so.1 dependency
-apt-get update && apt-get install -y libgl1-mesa-glx
 
 # Run the Streamlit app
 streamlit run --browser.serverAddress $SERVER_NAME --server.port $PORT app.py
